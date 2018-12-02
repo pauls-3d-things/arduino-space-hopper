d2ae583782b02c55c8922e9885d0940dd6d50274
Paul Smith <code@uvwxy.de>
2018-12-02 10:09:06 +0100

    Step 2: draw image

--- space-hopper.ino
+++ space-hopper.ino
@@ -2,6 +2,9 @@
 #include <Wire.h>
 #include <U8g2lib.h>
 
+// graphics
+#include "ship_0.h"
+
 U8G2_SSD1306_128X32_UNIVISION_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
 
 #define FRAME_RATE 8.0
@@ -15,7 +18,8 @@ void setup() {
 void loop() {
   u8g2.clearBuffer();
   u8g2.setFont(u8g2_font_profont12_tf);
-  u8g2.drawStr(10, 20, "space-hopper v1.0");
+  u8g2.drawStr(10, 10, "space-hopper v1.0");
+  u8g2.drawXBM(0, 16, 8, 8, ship_0_bits);
   u8g2.sendBuffer();
 
 }
