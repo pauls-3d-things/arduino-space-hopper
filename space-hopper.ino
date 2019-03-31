8c419d6f2e110883e95d0554e77b90972398a5d1
Paul Smith <code@uvwxy.de>
2019-03-31 13:11:32 +0200

    add rockets

--- space-hopper.ino
+++ space-hopper.ino
@@ -17,7 +17,10 @@
 #define SCREEN_WIDTH 128
 #define NUM_ASTEROIDS 12
 #define ASTEROID_MAX_SPEED 6
+#define ROCKET_SPEED 3
 
+// If you want to use a different screen
+// change the constructor here :), and defines above
 U8G2_SSD1306_128X32_UNIVISION_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
 
 enum GAMESTATE {
@@ -43,6 +46,32 @@ asteroid asteroids[NUM_ASTEROIDS];
 uint16_t asteroidCount = 0;
 uint16_t lastScore = 0;
 uint16_t highScore = 0;
+
+bool rocketFlying = false; // only one rocket
+uint16_t rocketX = SCREEN_WIDTH;
+uint16_t rocketY = SCREEN_HEIGHT;
+
+void drawRocket() {
+  // draw a pixel
+  u8g2.drawPixel(rocketX, rocketY);
+  u8g2.drawPixel(rocketX + 1, rocketY);
+  u8g2.drawPixel(rocketX + 2, rocketY);
+
+}
+
+void fireRocket() {
+  rocketFlying = true;
+  rocketX = SHIP_SIZE;
+  rocketY = shipY - SHIP_SIZE / 2; // center ship
+}
+
+void moveRocket() {
+  if (rocketX >= SCREEN_WIDTH) {
+    // stop the rocket at the screen edge
+    rocketFlying = false;
+  }
+  rocketX += ROCKET_SPEED;
+}
 
 void drawAsteroid(asteroid& a) {
   u8g2.drawXBM(a.x, a.y - ASTEROID_SIZE, ASTEROID_SIZE, ASTEROID_SIZE, asteroid_0_bits);
@@ -111,10 +140,13 @@ void boostShip() {
 }
 
 
-int buttonState = 0;
+int buttonState1 = 0;
+int buttonState2 = 0;
+
 void setup() {
   Serial.begin(9600);
   pinMode(D5, INPUT);
+  pinMode(D6, INPUT);
   u8g2.begin();
   u8g2.setContrast(0);
 }
@@ -131,9 +163,10 @@ void loop() {
 
     delay(250);
 
-    buttonState = digitalRead(D5);
-    if (buttonState == HIGH) {
+    buttonState1 = digitalRead(D5);
+    if (buttonState1 == HIGH) {
       gameState = RUNNING;
+      rocketFlying = false;
     }
     return;
   } else if (gameState == STOPPED) {
@@ -144,7 +177,7 @@ void loop() {
       asteroids[i].x = SCREEN_WIDTH;
       asteroids[i].s = 0;
     }
-   
+
     u8g2.clearBuffer();
     u8g2.setFont(u8g2_font_profont12_tf);
     u8g2.drawStr(24, 8, String("Highscore: " + String(highScore)).c_str());
@@ -154,28 +187,55 @@ void loop() {
 
     delay(250);
 
-    buttonState = digitalRead(D5);
-    if (buttonState == HIGH) {
+    buttonState1 = digitalRead(D5);
+    if (buttonState1 == HIGH) {
       gameState = START;
     }
     return;
   }
 
+  // main game loop starts here
+
+  // input handling for button D5 and D6
   loopCount++;
-  buttonState = digitalRead(D5);
-  if (buttonState == HIGH) {
+  buttonState1 = digitalRead(D5);
+  if (buttonState1 == HIGH) {
     boostShip();
   }
+
+  buttonState2 = digitalRead(D6);
+  if (buttonState2 == HIGH) {
+    // only
+    if (!rocketFlying) {
+      fireRocket();
+    }
+  }
+
   moveShip();
+  moveRocket();
 
   for (uint8_t i = 0; i < NUM_ASTEROIDS; i++) {
 
+    // collision detection vs ship
     if (asteroids[i].x <= SHIP_SIZE / 2        // asteroid is close to ship
         && abs(asteroids[i].y - shipY) <= SHIP_SIZE / 2 // and is a hit
        ) {
       gameState = STOPPED;
       lastScore = asteroidCount;
       asteroidCount = 0;
+    }
+
+    // collision detection vs rocket
+    if (rocketFlying) {
+      // check if rocket x/y is close to asteroid
+      if (asteroids[i].x > rocketX
+          && asteroids[i].x < rocketX + ASTEROID_SIZE
+          && asteroids[i].y > rocketY
+          && asteroids[i].y < rocketY + ASTEROID_SIZE) {
+        // we have hit
+        rocketFlying = false;
+        asteroids[i].x = -1;
+      }
     }
 
     uint8_t r = random(1, max(1, 2000 / (10 + asteroidCount))); // randomly add an asteroid
@@ -193,6 +253,10 @@ void loop() {
   drawShip();
   for (uint8_t i = 0; i < NUM_ASTEROIDS; i++) {
     drawAsteroid(asteroids[i]);
+  }
+
+  if (rocketFlying) {
+    drawRocket();
   }
 
   u8g2.sendBuffer();
